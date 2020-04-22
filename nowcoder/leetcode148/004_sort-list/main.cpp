0450ab4b3e883a4868e2140cbbbacb52806e87a8
tianluowen <1308145492@qq.com>
2020-04-22 21:31:39 +0800

    2020-04-22 update

--- nowcoder/leetcode148/004_sort-list/main.cpp
+++ nowcoder/leetcode148/004_sort-list/main.cpp
@@ -23,12 +23,93 @@ public:
             return head;
         }
         
+        int size;
+        int listLength = 0;
+        ListNode* left = nullptr;
+        ListNode* right = nullptr;
+        ListNode* currtNode = head;
+        // 定义一个头结点
+        ListNode dummyHead(0);
+        dummyHead.next = head;
+        ListNode* tail = nullptr;
+        
+        // 求出链表长度
+        while (currtNode != nullptr)
+        {
+            listLength++;
+            currtNode = currtNode->next;
+        }
+        
+        for (size = 1; size < listLength; size *= 2)
+        {
+            // 每一次开始的头结点
+            currtNode = dummyHead.next;
+            tail = &dummyHead;
             
+            while (currtNode != nullptr)
+            {
+                // 链表分三段，第三段下一次处理
+                left = currtNode;
+                right = cut(currtNode, size);
+                currtNode = cut(right, size);
+                
+                // 合并后的放在新链表的尾部 并将 tail 移动到最后一个结点
+                tail->next = merge(left, right);
+                while (tail->next != nullptr)
+                    tail = tail->next;
+            }
+        }
+    return dummyHead.next;
     }
-};
-
-
-
+    
+    // 分割链表
+    ListNode* cut(ListNode* head, int setp)
+    {
+        // 要注意将分割开的链表最后一个 next 置为空 所以先走 setp - 1 步
+        while (head != nullptr && --setp > 0)
+            head = head->next;
+        if (head == nullptr)
+            return head;
+        
+        auto temp = head->next;
+        head->next = nullptr;
+        return temp;
+    }
+    
+    // 合并链表
+    ListNode* merge(ListNode* left, ListNode* right)
+    {
+        ListNode dummyHead(0);
+        ListNode* currNode = &dummyHead;
+        while (left != nullptr && right != nullptr)
+        {
+            if (left->val < right->val)
+            {
+                currNode->next = left;
+                currNode = left;
+                left = left->next;
+            }
+            else
+            {
+                currNode->next = right;
+                currNode = right;
+                right = right->next;
+            }
+        }
+        
+        // 将剩余的链表链接到尾部
+        if (left == nullptr)
+        {
+            currNode->next = right;
+        }
+        else
+        {
+            currNode->next = left;
+        }
+        
+        return dummyHead.next;
+    }
+};            
 
 int main(void)
 {
---
+++ nowcoder/leetcode148/006_binary-tree-postorder-traversal/main.cpp
@@ -0,0 +1,87 @@
+// date: 2020-04-22
+// desc: 求给定的二叉树的后序遍历 
+//       备注；用递归来解这道题太没有新意了，可以给出迭代的解法么？
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+// Definition for binary tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> list;
+        stack<TreeNode> sta;
+        TreeNode tempNode(0);
+        
+        // 为空 返回空
+        if (root == nullptr)
+            return list;
+        
+        // 将根结点 入栈，依次取出，当某个结点无子节点时，将数据入 列表 否则 入栈
+        sta.push(*root);
+        while (!sta.empty())
+        {
+            tempNode = sta.top();
+            sta.pop();
+            list.insert(list.begin(), tempNode.val);
+            
+            // 注意插入 list 开始的位置 
+            if (tempNode.left != nullptr)
+            {
+                sta.push(*tempNode.left);
+            }
+            if (tempNode.right != nullptr)
+            {
+                sta.push(*tempNode.right);
+            }
+        }
+       
+        return list;
+    }
+};            
+        
+
+int main(void)
+{
+    Solution s;
+    vector<int> v;
+    
+    TreeNode* head = new TreeNode(2);
+    head->left = new TreeNode(4);
+    head->right = new TreeNode(3);
+    head->left->right = new TreeNode(1);
+    head->right->left = new TreeNode(6);
+
+    cout << "原来的树: " << endl;
+    cout << "    2" << endl;
+    cout << "4       3" << endl;;
+    cout << "  1  6" << endl;
+
+    v = s.postorderTraversal(head);
+
+    cout << "\n后序输出结果：\n";
+    cout << "[ ";
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        cout << v[i] << ", ";
+    }
+    cout << v[v.size() - 1] << " ]" << endl;
+
+    delete head->left->right;
+    delete head->right->left;
+    delete head->left;
+    delete head->right;
+    delete head;
+
+    return 0;
+}
